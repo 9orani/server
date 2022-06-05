cc9aa917489260e9c6ea0bd772db102deb293ce3
정현모 <gusah009@naver.com>
2022-06-05 20:48:03 +0900

    chore: clear git cache

---
+++ src/main/java/com/example/oidc/service/auth/SignInService.java
@@ -0,0 +1,10 @@
+package com.example.oidc.service.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SignInService {
+
+}
