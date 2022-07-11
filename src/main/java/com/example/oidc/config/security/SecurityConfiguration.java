a837d857e818e950f69746da2f05e72f631c92f1
정핸모 <gusah009@naver.com>
2022-07-12 08:05:18 +0900

    fix: API 문서 오타 수정 및 API 문서에 접근 권한 부여

--- src/main/java/com/example/oidc/config/security/SecurityConfiguration.java
+++ src/main/java/com/example/oidc/config/security/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authorizeRequests() // 다음 리퀘스트에 대한 사용권한 체크
-        .antMatchers("/*/auth/*", "/exception/*").permitAll() // 가입 및 인증 주소는 누구나 접근가능
+        .antMatchers("/*/auth/*", "/exception/*", "/docs/*").permitAll() // 가입 및 인증 주소는 누구나 접근가능
         .anyRequest().hasRole("USER") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
         .and()
         .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
