bdf22a34803431bf9f6f6883f6e4380ef855b34c
정핸모 <gusah009@naver.com>
2022-07-25 14:55:47 +0900

    test: JWT 유효성 검사 API test 제작
    issue: #4

--- src/test/java/com/example/oidc/controller/util/AuthControllerTest.java
+++ src/test/java/com/example/oidc/controller/util/AuthControllerTest.java
@@ -2,22 +2,161 @@ package com.example.oidc.controller.util;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.oidc.dto.auth.TokenValidRequestDto;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
 //@Transactional
 class AuthControllerTest extends AuthControllerTestSetup {
 
   private final Long userPk = 1L;
   private final List<String> roles = new ArrayList<>(List.of("ROLE_USER", "ROLE_TEST"));
+
+  @Test
+  @DisplayName("JWT 유효성 검사 성공")
+  public void checkJwtSuccess() throws Exception {
+    long validTime = 1000L * 60 * 60;
+    String validToken = jwtTokenProvider.
+        createToken(String.valueOf(userPk), roles, validTime);
+    TokenValidRequestDto content = TokenValidRequestDto.builder().token(validToken).build();
+
+    String docSuccess = "성공: true +\n실패: false";
+    String docCode = "성공할 경우 0\n";
+    String docMsg = "";
+    mockMvc.perform(post("/v1/jwt/check")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(asJsonString(content)))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.code").value(0))
+        .andExpect(jsonPath("$.data.valid").value(true))
+        .andExpect(jsonPath("$.data.tokenMsg").value("유효한 토큰입니다."))
+        .andExpect(jsonPath("$.data.token").value(validToken))
+        .andDo(document("check-jwt",
+            requestFields(
+                fieldWithPath("token").description("유저 토큰 ('Bearer'를 붙여야 합니다)")
+            ),
+            responseFields(
+                generateTokenValidResponseFields(ResponseType.SINGLE, docSuccess, docCode, docMsg)
+            )
+        ));
+  }
+
+  @Test
+  @DisplayName("JWT 유효성 검사 실패 - 토큰 없음")
+  public void checkJwtFailedByNoToken() throws Exception {
+    TokenValidRequestDto content = TokenValidRequestDto.builder().token("").build();
+
+    mockMvc.perform(post("/v1/jwt/check")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(asJsonString(content)))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.code").value(0))
+        .andExpect(jsonPath("$.data.valid").value(false))
+        .andExpect(jsonPath("$.data.tokenMsg").value("토큰이 비어있습니다."))
+        .andExpect(jsonPath("$.data.token").value(""));
+  }
+
+  @Test
+  @DisplayName("JWT 유효성 검사 실패 - 형식이 잘못된 토큰")
+  public void checkJwtFailedByInvalidTypeToken() throws Exception {
+    long validTime = 1000L * 60 * 60;
+    String validToken = jwtTokenProvider
+        .createToken(String.valueOf(userPk), roles, validTime);
+
+    String invalidToken = validToken.replace("Bearer", "OIDC");
+    TokenValidRequestDto content = TokenValidRequestDto.builder().token(invalidToken).build();
+
+    mockMvc.perform(post("/v1/jwt/check")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(asJsonString(content)))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.code").value(0))
+        .andExpect(jsonPath("$.data.valid").value(false))
+        .andExpect(jsonPath("$.data.tokenMsg").value("토큰 형식이 잘못되었습니다. 'Bearer 토큰' 형식으로 전달되어야 합니다."))
+        .andExpect(jsonPath("$.data.token").value(invalidToken));
+  }
+
+  @Test
+  @DisplayName("JWT 유효성 검사 실패 - 손상된 토큰")
+  public void checkJwtFailedByInvalidToken() throws Exception {
+    long validTime = 1000L * 60 * 60;
+    String validToken = jwtTokenProvider
+        .createToken(String.valueOf(userPk), roles, validTime);
+
+    String invalidToken = validToken.replace(".", "");
+    TokenValidRequestDto content = TokenValidRequestDto.builder().token(invalidToken).build();
+
+    mockMvc.perform(post("/v1/jwt/check")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(asJsonString(content)))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.code").value(0))
+        .andExpect(jsonPath("$.data.valid").value(false))
+        .andExpect(jsonPath("$.data.tokenMsg").value("손상된 토큰입니다."))
+        .andExpect(jsonPath("$.data.token").value(invalidToken));
+  }
+
+  @Test
+  @DisplayName("JWT 유효성 검사 실패 - 잘못된 시그니처 토큰")
+  public void checkJwtFailedByInvalidSignatureToken() throws Exception {
+    long validTime = 1000L * 60 * 60;
+    String validToken = jwtTokenProvider
+        .createToken(String.valueOf(userPk), roles, validTime);
+
+    String invalidToken = validToken.substring(0, validToken.length() - 3);
+    TokenValidRequestDto content = TokenValidRequestDto.builder().token(invalidToken).build();
+
+    mockMvc.perform(post("/v1/jwt/check")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(asJsonString(content)))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.code").value(0))
+        .andExpect(jsonPath("$.data.valid").value(false))
+        .andExpect(jsonPath("$.data.tokenMsg").value("시그니처 검증에 실패한 토큰입니다."))
+        .andExpect(jsonPath("$.data.token").value(invalidToken));
+  }
+
+  @Test
+  @DisplayName("JWT 유효성 검사 실패 - 만료된 토큰")
+  public void checkJwtFailedByExpiredToken() throws Exception {
+    long invalidTime = 0L;
+    String invalidToken = jwtTokenProvider
+        .createToken(String.valueOf(userPk), roles, invalidTime);
+    TokenValidRequestDto content = TokenValidRequestDto.builder().token(invalidToken).build();
+
+    mockMvc.perform(post("/v1/jwt/check")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(asJsonString(content)))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.code").value(0))
+        .andExpect(jsonPath("$.data.valid").value(false))
+        .andExpect(jsonPath("$.data.tokenMsg").value("만료된 토큰입니다."))
+        .andExpect(jsonPath("$.data.token").value(invalidToken));
+  }
 
   @Test
   @DisplayName("JWT로 권한 가져오기 성공")
